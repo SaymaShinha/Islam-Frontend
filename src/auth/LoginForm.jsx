4ec6b774e50beafda420bf3a58e202aa56da1916
SaymaShinha <sayma.shinha@gmail.com>
2026-06-07 22:57:09 +0600

    regitration and login

--- src/auth/LoginForm.jsx
+++ src/auth/LoginForm.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router";
-import Loading from "../components/Loading.jsx";
+import Loading from "../Components/Loading.jsx";
 import { useState } from "react";
 
 export default function LoginForm() {
