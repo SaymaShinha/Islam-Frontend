38ef6a901875429cf8c7e1fd729efa6a11041120
SaymaShinha <sayma.shinha@gmail.com>
2026-09-03 02:24:08 +0600

    audit fixes

--- src/Components/NavbarHeader.jsx
+++ src/Components/NavbarHeader.jsx
@@ -12,7 +12,7 @@ import {
 import { useEffect, useState } from "react";
 import { Link } from "react-router";
 import NamazNavList from "./NamazNavList.jsx";
-import UserProfileNavList from "./UserProfileNavList.jsx";
+// import UserProfileNavList from "./UserProfileNavList.jsx";
 
 function NavbarHeader() {
   const [theme, setTheme] = useState(
