fefb7a788c18ffb5c2b630d4e0fe86691ae4cb24
SaymaShinha <sayma.shinha@gmail.com>
2026-09-10 01:15:26 +0600

    Fix SEO import resolution

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router";
 import "./App.css";
-import NavbarHeader from "./Components/NavbarHeader.jsx";
-import WebFooter from "./Components/WebFooter.jsx";
+import NavbarHeader from "./components/NavbarHeader.jsx";
+import WebFooter from "./components/WebFooter.jsx";
 
 
 function App() {
